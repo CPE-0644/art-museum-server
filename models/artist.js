3553502fbbd3e6f92e7afb45ac504c860219577e
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-17 20:12:36 +0700

    refactored artist to class

--- models/artist.js
+++ models/artist.js
@@ -2,21 +2,20 @@ const _ = require('lodash');
 
 const data = require('../assets/database/artists.json').artists;
 
-function artist(json) {
-  // console.log(json);
-  let artist = {};
-  artist.id = json['artist-id'];
-  artist.name = json['name'];
-  artist.date_of_birth = json['dob'];
-  artist.date_of_died = json['dod'];
-  artist.country = json['country'];
-  artist.style = json['style'];
-  artist.artworks_id = json['artwork-id'];
-  return artist;
+class Artist {
+  constructor(json) {
+    this.id = json['artist-id'];
+    this.name = json['name'];
+    this.date_of_birth = json['dob'];
+    this.date_of_died = json['dod'];
+    this.country = json['country'];
+    this.style = json['style'];
+    this.artworks_id = json['artwork-id'];
+  }
 }
 
 let artists = [];
-_.forEach(data, json => artists.push(artist(json)));
+_.forEach(data, json => artists.push(new Artist(json)));
 
 module.exports = {
   artists
