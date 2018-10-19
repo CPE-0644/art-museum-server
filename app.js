cce8e9a9ea946288e4d7b5c194fb089b612c90fe
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-19 21:13:19 +0700

    fixed art-object type naming

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 
 const artistsRoute = require('./routes/artists');
@@ -7,6 +6,29 @@ const exhibitionRoute = require('./routes/exhibitions');
 const artworkRoute = require('./routes/artworks');
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'http://localhost:8081/#/artists'
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use('/api/artists', artistsRoute);
 app.use('/api/exhibitions', exhibitionRoute);
--- routes/artists.js
+++ routes/artists.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const cors = require('cors');
 const artistController = require('../controllers/artist');
 const artObjectController = require('../controllers/artwork');
 
